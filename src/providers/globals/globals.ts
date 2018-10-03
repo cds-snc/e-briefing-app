4e6dcfe23c3c3bdd407e60808183595abdef930e
Dave Samojlenko <dsamojlenko@gmail.com>
2018-10-03 12:57:07 -0400

    trip selection

--- src/providers/globals/globals.ts
+++ src/providers/globals/globals.ts
@@ -4,22 +4,24 @@ import 'rxjs/add/operator/map';
 import {Platform} from "ionic-angular";
 import {File} from "@ionic-native/file";
 
-
 @Injectable()
 export class GlobalsProvider {
 
     dataDirectory: string = '';
     api_key: string = 'API_KEY';
-    trip_id: any = 'TRIP_ID';
+    trip_id: string = 'TRIP_ID';
     api_url: string = 'https://ebrief.cds-snc.ca/api';
 
     constructor(public http: Http, private platform: Platform, private file: File) {
         platform.ready().then(() => {
+            if (localStorage.getItem('trip_id')) {
+                this.trip_id = localStorage.getItem('trip_id');
+            }
             // if we're on a device, use the app-writable data directory
             if (!this.platform.is('core')) {
                 this.setDataDirectory(this.file.dataDirectory ? this.file.dataDirectory : '');
             }
-        })
+        });
     }
 
     setDataDirectory(value) {
@@ -28,5 +30,14 @@ export class GlobalsProvider {
 
     getDataDirectory() {
         return this.dataDirectory;
+    }
+
+    setTripId(value) {
+        localStorage.setItem('trip_id', value);
+        this.trip_id = value;
+    }
+
+    getTripId() {
+        return this.trip_id;
     }
 }
